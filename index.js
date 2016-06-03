eafb5ccec3de379d4fea260c9ac4ae0387cd71c8
miko_shu <452753617@qq.com>
2016-06-03 15:02:17 +0800

    update

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ module.exports = function(content,file,setting){
   	var parser = new ssi(process.cwd(),process.cwd(),file.rExt,true)
   	var data = parser.parse(path.join(process.cwd(),file.subpath),content)
   	file.setContent(data.contents)
+  	return data.contents
 }
