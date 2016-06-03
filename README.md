7c6b9b2817cc415b456eb70c5b93ca927ca8a5fa
miko_shu <452753617@qq.com>
2016-06-03 11:39:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# fis-parser-ssi 使用文档
+
+## 安装
+
++ npm install fis-parser-ssi
+
+## 使用方法
+
+	fis.match('*.html', {
+	  parser: fis.plugin('ssi')
+	})
+
---
+++ index.js
@@ -0,0 +1,7 @@
+var ssi = require('ssi')
+var path = require('path')
+module.exports = function(content,file,setting){
+  	var parser = new ssi(process.cwd(),process.cwd(),file.rExt,true)
+  	var data = parser.parse(path.join(process.cwd(),file.subpath),content)
+  	file.setContent(data.contents)
+}
